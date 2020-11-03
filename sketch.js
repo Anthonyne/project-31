c8cd0d07a377b2cbdf159b4596c027a56fd9e1f0
Anthonyne <71289630+Anthonyne@users.noreply.github.com>
2020-11-03 15:47:48 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,57 +1,79 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
 
-var engine, world;
-var box1, block1;
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
 
     
-    ground = new Ground(600,height,1200,20)
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    block1 = new block(810, 350);
-    thin1 = new thin(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    block3 = new block(810, 220);
-
-    thin3 =  new thin(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    thin4 = new thin(760,120,150, PI/7);
-    thin5 = new thin(870,120,150, -PI/7);
-
-    ball = new ball(100,100);
-
+    
 }
+ 
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    console.thin(box2.body.position.x);
-    console.thin(box2.body.position.y);
-    console.thin(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    block1.display();
-    thin1.display();
 
-    box3.display();
-    box4.display();
-    block3.display();
-    thin3.display();
-
-    box5.display();
-    thin4.display();
-    thin5.display();
-
-    bird.display();
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
 }
